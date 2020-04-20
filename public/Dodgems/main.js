dc4360ae6f578c3b725157d02030c8b96bcd439d
Pierce <piercemiller23@gmail.com>
2020-04-20 16:23:07 +1200

    drawImage error fix

--- public/Dodgems/main.js
+++ public/Dodgems/main.js
@@ -58,49 +58,48 @@ pipe[0] = {
 
 function draw(){
     
-    ctx.drawImage(bg,0,0);
+    if(cvs.width && cvs.height){
+        ctx.drawImage(bg,0,0);
     
-    for(var i = 0; i < pipe.length; i++){
-        
-        constant = wallNorth.height+gap;
-        ctx.drawImage(wallNorth,pipe[i].x,pipe[i].y);
-        ctx.drawImage(wallSouth,pipe[i].x,pipe[i].y+constant);
-             
-        pipe[i].x--;
-        
-        if( pipe[i].x == 125 ){
-            pipe.push({
-                x : cvs.width,
-                y : Math.floor(Math.random()*wallNorth.height)-wallNorth.height
-            }); 
-        }
+        for(var i = 0; i < pipe.length; i++){
+            
+            constant = wallNorth.height+gap;
+            ctx.drawImage(wallNorth,pipe[i].x,pipe[i].y);
+            ctx.drawImage(wallSouth,pipe[i].x,pipe[i].y+constant);
+                 
+            pipe[i].x--;
+            
+            if( pipe[i].x == 125 ){
+                pipe.push({
+                    x : cvs.width,
+                    y : Math.floor(Math.random()*wallNorth.height)-wallNorth.height
+                }); 
+            }
+    
+            // detect collision
+            
+            if( bX + bird.width >= pipe[i].x && bX <= pipe[i].x + wallNorth.width && (bY <= pipe[i].y + wallNorth.height || bY+bird.height >= pipe[i].y+constant) || bY + bird.height >=  cvs.height - fg.height){
+                location.reload(); // reload the page
+            }
+            
+            if(pipe[i].x == 5){
+                score++;
+                scor.play();
+            }    
+        }   
 
-        // detect collision
+        ctx.drawImage(fg,0,cvs.height - fg.height);
         
-        if( bX + bird.width >= pipe[i].x && bX <= pipe[i].x + wallNorth.width && (bY <= pipe[i].y + wallNorth.height || bY+bird.height >= pipe[i].y+constant) || bY + bird.height >=  cvs.height - fg.height){
-            location.reload(); // reload the page
-        }
+        ctx.drawImage(bird,bX,bY);
         
-        if(pipe[i].x == 5){
-            score++;
-            scor.play();
-        }
+        bY += gravity;
         
+        ctx.fillStyle = "#FFFFFF";
+        ctx.font = "20px Minecraftia";
+        ctx.fillText("Score : "+score,10,cvs.height-20);
         
+        requestAnimationFrame(draw);
     }
-
-    ctx.drawImage(fg,0,cvs.height - fg.height);
-    
-    ctx.drawImage(bird,bX,bY);
-    
-    bY += gravity;
-    
-    ctx.fillStyle = "#FFFFFF";
-    ctx.font = "20px Minecraftia";
-    ctx.fillText("Score : "+score,10,cvs.height-20);
-    
-    requestAnimationFrame(draw);
-    
 }
 
 draw();
